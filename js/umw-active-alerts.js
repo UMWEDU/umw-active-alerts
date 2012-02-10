eaaa43d61f9c1f7a4c5c8b0e2eadef2e99c08ec1
cgrymala <cgrymala@5346cacd-8ae3-42ad-958d-fc27c7a1ea7b>
2012-02-10 21:27:55 +0000

    Tag 0.4 implements emergency/non-emergency announcements Modified JavaScript a bit in trunk to append a timestamp to AJAX call, to keep results from being cached.
    git-svn-id: svn+ssh://ftp.ten-321.com/home/cgrymala/svn/umw-active-alerts/trunk@10 5346cacd-8ae3-42ad-958d-fc27c7a1ea7b

--- js/umw-active-alerts.js
+++ js/umw-active-alerts.js
@@ -2,26 +2,26 @@ jQuery( function( $ ) {
 	/**
 	 * Perform the JSON request to check alert status
 	 */
-	$.getJSON( umwActAlerts.ajaxurl, {'action':'check_active_alert'}, function( result ) {
-		if( typeof( console ) !== 'undefined' ) {
+	$.getJSON( umwActAlerts.ajaxurl, { 'action':'check_active_alert', 'v':new Date().getTime() }, function( result ) {
+		/*if( typeof( console ) !== 'undefined' ) {
 			console.log( result );
-		}
+		}*/
 		
 		if ( 'emergency' in result ) {
-			if ( typeof( console ) !== 'undefined' ) {
+			/*if ( typeof( console ) !== 'undefined' ) {
 				console.log( 'Getting ready to insert an emergency alert' );
-			}
+			}*/
 			insert_emergency_alert( result.emergency );
-		} else if ( typeof( console ) !== 'undefined' ) {
-			console.log( 'No emergency alert was found' );
+		/*} else if ( typeof( console ) !== 'undefined' ) {
+			console.log( 'No emergency alert was found' );*/
 		}
 		if ( 'alert' in result ) {
-			if ( typeof( console ) !== 'undefined' ) {
+			/*if ( typeof( console ) !== 'undefined' ) {
 				console.log( 'Getting ready to insert an alert' );
-			}
+			}*/
 			insert_active_alert( result.alert );
-		} else if ( typeof( console ) !== 'undefined' ) {
-			console.log( 'No alert was found' );
+		/*} else if ( typeof( console ) !== 'undefined' ) {
+			console.log( 'No alert was found' );*/
 		}
 	} );
 	
@@ -32,9 +32,9 @@ jQuery( function( $ ) {
 	function insert_emergency_alert( data ) {
 	
 		if( typeof( data.html ) === 'undefined' || -1 == data.html || '0' == data.html || null == data.html ) {
-			if( typeof( console ) !== 'undefined' ) {
+			/*if( typeof( console ) !== 'undefined' ) {
 				console.log( 'No emergency alert defined' );
-			}
+			}*/
 			return;
 		}
 		
@@ -65,22 +65,22 @@ jQuery( function( $ ) {
 	 */
 	function insert_active_alert( data ) {
 		if( typeof( data.html ) === 'undefined' || -1 == data.html || '0' == data.html || null == data.html ) {
-			if( typeof( console ) !== 'undefined' ) {
+			/*if( typeof( console ) !== 'undefined' ) {
 				console.log( 'No alert defined' );
-			}
+			}*/
 			return;
 		}
 		if ( $( '.home-top-left #eps-slideshow-caption' ).length <= 0 ) {
-			if ( typeof( console ) !== 'undefined' ) {
+			/*if ( typeof( console ) !== 'undefined' ) {
 				console.log( 'We did not find #eps-slideshow-caption inside .home-top-left' );
-			}
+			}*/
 			return;
 		}
 		var $umwah = $( data.html );
 		$umwah.hide().appendTo( $( '.home-top-left' ) ).fadeIn( 1000 );
-		if ( typeof( console ) !== 'undefined' ) {
+		/*if ( typeof( console ) !== 'undefined' ) {
 			console.log( $umwah );
 			console.log( 'We should have just appended the object logged above to .home-top-left' );
-		}
+		}*/
 	}
 } );
