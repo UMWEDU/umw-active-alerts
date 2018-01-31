b6078e124d2f847afc16eca341986eacdd98633d
Curtiss Grymala <cgrymala@umw.edu>
2018-01-31 10:26:05 -0500

    Begin rewriting plugin to use ACF and hardcoded post type registration, rather than relying on Types

---
+++ classes/class-umw-advisories-plugin.php
@@ -0,0 +1,204 @@
+<?php
+namespace {
+    if ( ! defined( 'ABSPATH' ) ) {
+        die( 'You do not have permission to access this file directly.' );
+    }
+}
+
+namespace UMW_Advisories {
+    if ( ! class_exists('Plugin') ) {
+        class Plugin {
+            /**
+             * @var \UMW_Advisories\Plugin $instance holds the single instance of this class
+             * @access private
+             */
+            private static $instance;
+
+            /**
+             * @var string $version holds the version number for the plugin
+             * @access public
+             */
+            public static $version = '2018.1';
+
+	        /**
+	         * @var bool $is_root whether this is the root site of the UMW system or not
+	         * @access private
+	         */
+	        private $is_root = false;
+
+	        /**
+	         * @var string $root_url the URL to the root UMW site
+	         * @access private
+	         */
+	        private $root_url = null;
+
+	        /**
+	         * @var bool $is_alerts whether this is the main Advisories site or not
+	         * @access private
+	         */
+	        private $is_alerts = false;
+
+	        /**
+	         * @var string $alerts_url the URL to the main Advisories site
+	         * @access private
+	         */
+	        private $alerts_url = null;
+
+            /**
+             * Creates the \AdmLdg17\Plugin object
+             *
+             * @access private
+             * @since  0.1
+             */
+            private function __construct() {
+	            $this->is_root();
+	            $this->is_advisories();
+	            $this->setup_acf();
+            }
+
+            /**
+             * Returns the instance of this class.
+             *
+             * @access  public
+             * @since   0.1
+             * @return  \UMW_Advisories\Plugin
+             */
+            public static function instance() {
+                if ( ! isset( self::$instance ) ) {
+                    $className      = __CLASS__;
+                    self::$instance = new $className;
+                }
+
+                return self::$instance;
+            }
+
+            /**
+             * Determines whether this is the root UMW site
+             * Also determines the URL to the root UMW site
+             *
+             * @uses UMW_IS_ROOT
+             * @uses \UMW_Advisories\Plugin::$is_root
+             * @uses \UMW_Advisories\Plugin::$root_url
+             *
+             * @access private
+             * @since  1.0
+             * @return void
+             */
+            private function is_root() {
+	            if ( defined( 'UMW_IS_ROOT' ) ) {
+		            if ( is_numeric( UMW_IS_ROOT ) && $GLOBALS['blog_id'] == UMW_IS_ROOT ) {
+			            $this->is_root = true;
+			            $this->root_url = get_bloginfo( 'url' );
+		            } else if ( is_numeric( UMW_IS_ROOT ) ) {
+			            $this->root_url = get_blog_option( UMW_IS_ROOT, 'home_url', null );
+		            } else {
+			            $this->root_url = esc_url( UMW_IS_ROOT );
+		            }
+	            }
+            }
+
+            /**
+             * Determine whether this is the main Advisories site or not
+             * Also determines the URL to the main Advisories site
+             *
+             * @uses UMW_ADVISORIES_SITE
+             * @uses \UMW_Advisories\Plugin\$is_alerts
+             * @uses \UMW_Advisories\Plugin\$alerts_url
+             * @uses \UMW_Advisories\Plugin\setup_alerts_site()
+             * @uses \UMW_Advisories\Plugin\add_syndication_actions()
+             *
+             * @access private
+             * @since  1.0
+             * @return void
+             */
+            private function is_advisories() {
+            	if ( ! defined( 'UMW_ADVISORIES_SITE' ) )
+            		return;
+
+	            if ( is_numeric( UMW_ADVISORIES_SITE ) ) {
+		            if ( UMW_ADVISORIES_SITE == $GLOBALS['blog_id'] ) {
+			            $this->is_alerts = true;
+			            $this->alerts_url = esc_url( get_bloginfo( 'url' ) );
+			            $this->setup_alerts_site();
+		            } else {
+			            $this->is_alerts = false;
+			            $this->alerts_url = esc_url( get_blog_option( UMW_ADVISORIES_SITE, 'home' ) );
+			            $this->add_syndication_actions();
+		            }
+	            } else {
+		            $this->is_alerts = false;
+		            $this->alerts_url = esc_url( UMW_ADVISORIES_SITE );
+		            $this->add_syndication_actions();
+	            }
+            }
+
+	        /**
+	         * Setup the necessary filters/includes for use with ACF
+	         *
+	         * @access private
+	         * @since  2018.1
+	         * @return void
+	         */
+	        private function setup_acf() {
+	        	add_filter( 'acf/settings/path', function() { return plugin_dir_path( __FILE__ ) . '/includes/acf/'; } );
+	        	add_filter( 'acf/settings/dir', function() { return plugin_dir_url( __FILE__ ) . '/includes/acf/'; } );
+	        	if ( ! is_plugin_active( 'advanced-custom-fields-pro' ) && ( is_multisite() && ! is_plugin_active_for_network( 'advanced-custom-fields-pro' ) ) ) {
+	        		add_filter( 'acf/settings/show_admin', '__return_false' );
+		        }
+		        include_once( plugin_dir_path( __FILE__ ) . '/includes/acf/acf.php' );
+	        	include_once( plugin_dir_path( __FILE__ ) . '/includes/acf-fields.php' );
+	        }
+
+            /**
+             * Setup any actions/filters that need to be registered on the
+             * 		main Advisories site
+             *
+             * @access private
+             * @since  1.0
+             * @return void
+             */
+            private function setup_alerts_site() {
+	            add_action( 'rest_api_init', array( $this, 'bypass_cas' ) );
+            }
+
+	        /**
+	         * Setup any syndication actions that need to be handled
+	         */
+	        private function add_syndication_actions() {
+		        add_action( 'save_post_advisory', array( $this, 'push_advisory' ), 10, 2 );
+		        add_action( 'wp_trash_post', array( $this, 'trash_advisory' ) );
+		        add_action( 'untrashed_post', array( $this, 'untrash_advisory' ) );
+		        add_action( 'delete_post', array( $this, 'delete_advisory' ) );
+
+		        $this->register_post_types();
+	        }
+
+	        /**
+	         * Register the necessary post types and custom fields for this plugin
+	         */
+	        private function register_post_types() {
+	        	if ( $this->is_alerts ) {
+	        		require_once( plugin_dir_path( __FILE__ ) . '/includes/post-types-root.php' );
+		        } else {
+	        		require_once( plugin_dir_path( __FILE__ ) . '/includes/post-types-remote.php' );
+		        }
+	        }
+
+            /**
+             * If we are attempting to perform an authenticated REST API request,
+             *      we need to bypass the CAS authentication and go straight to
+             *      WordPress native authentication
+             *
+             * @uses WPCAS_BYPASS
+             *
+             * @access public
+             * @since  1.0
+             * @return void
+             */
+            public function bypass_cas() {
+	            if ( ! defined( 'WPCAS_BYPASS' ) )
+		            define( 'WPCAS_BYPASS', true );
+            }
+        }
+    }
+}
---
+++ classes/includes/acf-fields.php
@@ -0,0 +1,151 @@
+<?php
+if( function_exists('acf_add_local_field_group') ):
+
+	acf_add_local_field_group(array(
+		'key' => 'group_5a71d775061ac',
+		'title' => 'Advisory Expiration',
+		'fields' => array(
+			array(
+				'key' => 'field_5a71d791bceef',
+				'label' => 'When should this advisory expire?',
+				'name' => '_advisory_expires_time',
+				'type' => 'date_time_picker',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'display_format' => 'F j, Y g:i a',
+				'return_format' => 'Y-m-d H:i:s',
+				'first_day' => 0,
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'advisory',
+				),
+			),
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'alert',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => array(
+			0 => 'custom_fields',
+			1 => 'discussion',
+			2 => 'comments',
+			3 => 'format',
+			4 => 'page_attributes',
+			5 => 'featured_image',
+			6 => 'categories',
+			7 => 'tags',
+			8 => 'send-trackbacks',
+		),
+		'active' => 1,
+		'description' => '',
+	));
+
+	acf_add_local_field_group(array(
+		'key' => 'group_5a71d9ab71516',
+		'title' => 'Advisory Information',
+		'fields' => array(
+			array(
+				'key' => 'field_5a71da4225611',
+				'label' => 'When should this advisory expire?',
+				'name' => '_advisory_expires_time',
+				'type' => 'date_time_picker',
+				'instructions' => 'If you do not want this advisory to appear as an active advisory, set the expiration date in the past.',
+				'required' => 1,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'display_format' => 'F j, Y g:i a',
+				'return_format' => 'Y-m-d H:i:s',
+				'first_day' => 0,
+			),
+			array(
+				'key' => 'field_5a71da7525612',
+				'label' => 'What is the original URL of this advisory?',
+				'name' => '_advisory_permalink',
+				'type' => 'url',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+			),
+			array(
+				'key' => 'field_5a71da9125613',
+				'label' => 'Who is the original author of this advisory?',
+				'name' => '_advisory_author',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'external_advisory',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => array(
+			0 => 'excerpt',
+			1 => 'custom_fields',
+			2 => 'discussion',
+			3 => 'comments',
+			4 => 'revisions',
+			5 => 'author',
+			6 => 'format',
+			7 => 'page_attributes',
+			8 => 'featured_image',
+			9 => 'categories',
+			10 => 'tags',
+			11 => 'send-trackbacks',
+		),
+		'active' => 1,
+		'description' => '',
+	));
+
+endif;
