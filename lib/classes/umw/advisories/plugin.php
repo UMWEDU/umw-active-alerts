93ef2f157842689c62afe5fd16e650fc0f600ecf
Curtiss Grymala <cgrymala@umw.edu>
2020-03-13 14:52:34 -0400

    Tweak JS

--- lib/classes/umw/advisories/plugin.php
+++ lib/classes/umw/advisories/plugin.php
@@ -19,7 +19,7 @@ namespace UMW\Advisories {
 			 * @var string $version holds the version number for the plugin
 			 * @access public
 			 */
-			public static $version = '2020.03.13.16';
+			public static $version = '2020.03.13.17';
 
 			/**
 			 * @var bool $is_root whether this is the root site of the UMW system or not
